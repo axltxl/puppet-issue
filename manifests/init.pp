3387c1baa263b3a4992ff4f82a340db8be5b0341
Alejandro Ricoveri <alejandro.ricoveri@blanclink.com>
2014-12-03 19:16:37 -0430

    Initial import

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+class issue (
+  $file => undef
+) {
+  # Issue banner
+  file { '/etc/issue.net':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => $file
+  }
+}
