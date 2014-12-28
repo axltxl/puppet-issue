ccaa97bf31276a7e05a2c02496e9547e6b724265
Alejandro Ricoveri <alejandro.ricoveri@blanclink.com>
2014-12-28 15:02:05 -0430

    Proper class documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,27 @@
+# == Class: dumber
+#
+# Wrapper class to handle issue files
+#
+# === Parameters
+#
+# [*source*]
+#   Source file to be used
+#
+# === Examples
+#
+#  class { 'issue':
+#    source => 'puppet://files/my_issue.txt'
+#  }
+#
+# === Authors
+#
+# Alejandro Ricoveri <alejandroricoveri@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2014 Alejandro Ricoveri.
+#
+
 class issue (
   $file = undef
 ) {
